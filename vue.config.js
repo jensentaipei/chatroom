0af57607a5813a52a83f332cead440410c79f68c
jensen <jensentaipei@gmail.com>
2021-03-17 13:48:46 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ const path = require('path')
 const vantTheme = path.resolve(__dirname, "./src/assets/less/vantChange.less")
 
 module.exports = {
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/chatroom/'
+    : '/',
   productionSourceMap: false,
   css: {
     loaderOptions: {
