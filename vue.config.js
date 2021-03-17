4441b1313ab406d1db88f447a105e5df56161ac9
jensen <jensentaipei@gmail.com>
2021-03-17 10:41:05 +0800

    aa

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const path = require('path')
 const vantTheme = path.resolve(__dirname, "./src/assets/less/vantChange.less")
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/chat/'
+  : '/',
     productionSourceMap: false,
     css: {
         loaderOptions: {
