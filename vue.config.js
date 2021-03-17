0d213e0afa847bf26c70ca279dde2d98a666c9f2
jensen <jensentaipei@gmail.com>
2021-03-17 13:42:28 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -2,24 +2,21 @@ const path = require('path')
 const vantTheme = path.resolve(__dirname, "./src/assets/less/vantChange.less")
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/chat/'
-  : '/',
-    productionSourceMap: false,
-    css: {
-        loaderOptions: {
-        less: {
-            modifyVars: {
-                // 可在此编写样式变量
-                // 'blue': 'red',
-                // 也可以引入我们创建好的theme.less文件
-                hack: `true; @import "${vantTheme}";`,
-            },
-            javascriptEnabled: true
+  publicPath: './',
+  productionSourceMap: false,
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          // 可在此编写样式变量
+          // 'blue': 'red',
+          // 也可以引入我们创建好的theme.less文件
+          hack: `true; @import "${vantTheme}";`,
         },
+        javascriptEnabled: true
+      },
     },
   }
-
 }
 
 function resolve (dir) {
